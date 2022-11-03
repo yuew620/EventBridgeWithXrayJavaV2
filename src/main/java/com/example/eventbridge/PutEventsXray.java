9dd2bc5c685800cf8a5658b0d0cffe283fc869a4
Wang <yyuewang@88665a07566a.ant.amazon.com>
2022-11-03 21:11:06 +0800

    add file

---
+++ src/main/java/com/example/eventbridge/PutEventsXray.java
@@ -0,0 +1,117 @@
+//snippet-sourcedescription:[PutEvents.java demonstrates how to send custom events to Amazon EventBridge.]
+//snippet-keyword:[AWS SDK for Java v2]
+//snippet-service:[Amazon EventBridge]
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.example.eventbridge;
+
+// snippet-start:[eventbridge.java2._put_event.import]
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClient;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.amazonaws.xray.interceptors.TracingInterceptor;
+// snippet-end:[eventbridge.java2._put_event.import]
+
+/**
+ * Before running this Java V2 code example, set up your development environment, including your credentials.
+ *
+ * For more information, see the following documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class PutEventsXray {
+
+    public static void main(String[] args) {
+
+        final String usage =
+            "To run this example, supply two resources, identified by Amazon Resource Name (ARN), which the event primarily concerns. " +
+            "Any number, including zero, may be present. \n" +
+            "For example: PutEvents <resourceArn> <resourceArn2>\n";
+
+        if (args.length != 2) {
+            System.out.println(usage);
+            System.exit(1);
+        }
+
+        String resourceArn = args[0];
+        String resourceArn2 = args[1];
+        Region region = Region.US_EAST_1;
+
+
+        EventBridgeClient eventBrClient = EventBridgeClient.builder()
+            .region(region)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
+
+        EventBridgeAsyncClient eventBrAscClient = EventBridgeAsyncClient.builder()
+                .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                                    .overrideConfiguration(ClientOverrideConfiguration.builder()
+                            // manual instrumentation as described here https://aws.amazon.com/blogs/developer/x-ray-support-for-the-aws-sdk-for-java-v2/
+                            .addExecutionInterceptor(new TracingInterceptor())
+                            .build())
+                .build();
+
+
+//        @Produces
+//        public S3AsyncClient asyncS3Client(@ConfigProperty(name = "aws.region", defaultValue = "us-west-2") final String awsRegion) {
+//            return S3AsyncClient.builder()
+//                    .region(Region.of(awsRegion))
+//                    .httpClient(NettyNioAsyncHttpClient.builder().build())
+//                    .overrideConfiguration(ClientOverrideConfiguration.builder()
+//                            // manual instrumentation as described here https://aws.amazon.com/blogs/developer/x-ray-support-for-the-aws-sdk-for-java-v2/
+//                            .addExecutionInterceptor(new TracingInterceptor())
+//                            .build())
+//                    .build();
+//        }
+
+        putEBEvents(eventBrClient, resourceArn, resourceArn2);
+        eventBrClient.close();
+    }
+
+    // snippet-start:[eventbridge.java2._put_event.main]
+    public static void putEBEvents(EventBridgeClient eventBrClient, String resourceArn, String resourceArn2 ) {
+
+        try {
+            // Populate a List with the resource ARN values.
+            List<String> resources = new ArrayList<>();
+            resources.add(resourceArn);
+            resources.add(resourceArn2);
+
+            PutEventsRequestEntry reqEntry = PutEventsRequestEntry.builder()
+                .resources(resources)
+                .source("com.mycompany.myapp")
+                .detailType("myDetailType")
+                .detail("{ \"key1\": \"value1\", \"key2\": \"value2\" }")
+                .build();
+
+            PutEventsRequest eventsRequest = PutEventsRequest.builder()
+                .entries(reqEntry)
+                .build();
+
+            PutEventsResponse result = eventBrClient.putEvents(eventsRequest);
+            for (PutEventsResultEntry resultEntry : result.entries()) {
+                if (resultEntry.eventId() != null) {
+                    System.out.println("Event Id: " + resultEntry.eventId());
+                } else {
+                    System.out.println("Injection failed with Error Code: " + resultEntry.errorCode());
+                }
+            }
+
+        } catch (EventBridgeException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+    // snippet-end:[eventbridge.java2._put_event.main]
+}
